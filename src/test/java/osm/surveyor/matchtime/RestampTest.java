580ec5dcab720780576556d770a2c09078127558
haya4 <hayashi.yuu@gmail.com>
2020-05-16 22:07:21 +0900

    sss

--- src/test/java/osm/surveyor/matchtime/RestampTest.java
+++ src/test/java/osm/surveyor/matchtime/RestampTest.java
@@ -2,8 +2,8 @@ package osm.surveyor.matchtime;
 
 import static org.junit.Assert.*;
 
-import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.junit.experimental.theories.DataPoints;
@@ -25,35 +25,29 @@ public class RestampTest {
     @Theory
     public void パラメータテスト(Fixture dataset) {
     	try {
-    		System.out.println(dataset.toString());
-            RestampTest.setUp();
-            dataset.setUp();
-            Restamp.main(dataset.args);
-            dataset.check();
-            dataset.checkUnchanged();
-            RestampTest.tearDown();
+            // カメラディレクトリを作成する
+    		Path tgz = Paths.get("target/test-classes/images.tar.gz");
+    		if (Files.exists(tgz)) {
+                UnZip.uncompress(Paths.get("target/test-classes/images.tar.gz"), Paths.get("target/test-classes/"));
+
+                // OUTディレクトリを作成する
+                Files.createDirectories(Paths.get("target/test-classes/out"));
+
+                dataset.setUp();
+                Restamp.main(dataset.args);
+                dataset.check();
+                dataset.checkUnchanged();
+                
+                // IMGディレクトリを削除する
+                UnZip.delete(Paths.get("target/test-classes/images"));
+                
+                // OUTディレクトリを削除する
+                UnZip.delete(Paths.get("target/test-classes/out"));
+    		}
     	}
     	catch(Exception e) {
-            e.printStackTrace();
-            fail("Exceptionが発生した。");
+            fail(e.toString());
     	}
     }
-    
-    static void setUp() throws IOException {
-        // カメラディレクトリを作成する
-        UnZip.uncompress(Paths.get("target/test-classes/images.tar.gz"), Paths.get("target/test-classes/"));
-
-        // OUTディレクトリを作成する
-        Files.createDirectories(Paths.get("target/test-classes/out"));
-    }
-    
-    static void tearDown() throws IOException {
-        // IMGディレクトリを削除する
-        UnZip.delete(Paths.get("target/test-classes/images"));
-        
-        // OUTディレクトリを削除する
-        UnZip.delete(Paths.get("target/test-classes/out"));
-    }
-    
     
 }
